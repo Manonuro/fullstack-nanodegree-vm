4395b6d6927bd2408913d6463622040f2d987828
Manonuro <mnochb@gmail.com>
2019-11-29 20:42:04 -0500

    Rewrite flask import libraries

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for,\
-    flash, jsonify, abort, g
+from flask import (
+   Flask,
+   render_template,
+   request,
+   redirect,
+   url_for,
+   flash,
+   jsonify,
+   abort,
+   g
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, sessionmaker
